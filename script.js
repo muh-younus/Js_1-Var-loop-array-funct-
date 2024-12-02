cc1bb37f8e003ecc8e6508fafba003fd501ff3fb
muh-younus <braq369@gmail.com>
2024-12-02 23:51:30 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let number1 = prompt("Please Enter first number");
+let number2 = prompt("Enter second number");
+let number3 = prompt("Enter thired number");
+
+function add(num1, num2, num3){
+
+    let add_number = num1 + num2 + num3;
+    return add_number;
+
+}
+
+var store_value = add(num1,num2,num3);
